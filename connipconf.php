14eaabe0d41d72cab7ba2b210607bd8a6a0db314
a0938383383 <a0938383383@gmail.com>
2018-07-17 15:17:56 +0800

    Delete connipconf.php

--- connipconf.php
+++
@@ -1,8 +0,0 @@
-<?php
-$serverhostip = "168.2.9.22"; // server ip
-$serverfolder = "/multi_upload_txt";// upload
-$count1 = "/joblist.php?sid=0";// 生管
-$count2 = "/joblistmc.php?sid=0";// 現場
-$count3 = "/history.php?sid=0";// 現場
-$count4 = "/management/index.php"//　人員
-?>
